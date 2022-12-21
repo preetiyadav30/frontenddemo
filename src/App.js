71305346c1caa685f3502b212a9eb756b6bad797
YogeshGurjar011 <yogesh@rslsofttech.com>
2022-12-21 18:46:02 +0530

    this is commit to yogesh

--- src/App.js
+++ src/App.js
@@ -7,7 +7,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> 
+          Welcome To Our CICD DEMO With Github Actions
+          For RSL 
         </p>
         <a
           className="App-link"
